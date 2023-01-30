9acf18eff31473281153d1aa8c342ad70dd9f1f5
dluman <dluman@allegheny.edu>
2023-01-30 15:51:46 +0000

    Adding progress on workbench

--- PowerTools/src/main.py
+++ PowerTools/src/main.py
@@ -18,20 +18,29 @@ import suffix
 
 # TODO: Write function to calculate result of various numbers 
 #       raised to various powers.
+# Function signature
+def raise_to_power(number: float = 0.0, power: float = 1) -> float:
+    raised_num = number ** power
+    # Sends the number back to the place we left off
+    return raised_num
 
 # TODO: Write function to calculates nth root of a number 
 #       by raising to fractional power
 
-# TODO: Print program introduction
+# Print program introduction
 print("Power and root calculator", end = "\n\n")
 
 # TODO: Collect user information
+user_num = int(input("Enter a base: "))
+user_pow = int(input("Enter an exponent: "))
 
 # TODO: Perform calculations using functions
+# Function call
+raised = raise_to_power(number = user_num, power = user_pow)
 
 # TODO: Use suffix to get the proper number suffix: 1st, 2nd...
 num_suffix = suffix.determine(user_pow)
 
 # TODO: Print results for both computations using same power
-print(f"{user_num} raised to the {user_pow}{num_suffix} power: {raised}")
-print(f"{user_num} taken to the {user_pow}{num_suffix} root: {rooted}")
+# print(f"{user_num} raised to the {user_pow}{num_suffix} power: {raised}")
+# print(f"{user_num} taken to the {user_pow}{num_suffix} root: {rooted}")
